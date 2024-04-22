6ccb4f035b6eecc1ba94d84f78ecc5647bd2507f
annejunkes <anneburkhardtjunkes@gmail.com>
2024-04-21 21:48:25 -0300

    Dia 2

---
+++ DayTwo/src/ifsc/Ex3.java
@@ -0,0 +1,21 @@
+package ifsc;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		
+		String[][] matriz = new String[10][10];
+
+		for(int i=0; i<matriz.length; i++) {
+			
+			for(int j=0; j<matriz[i].length; j++) {
+				
+				System.out.print(" - ");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
